1cf122a45d5a903d9153095cb33cd55ebc66efcb
= <proozeboom@gmail.com>
2022-03-04 17:09:04 +0100

    prepare release

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -17,6 +17,9 @@ use Symplify\MonorepoBuilder\ValueObject\Option;
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     
+    // main branch
+    $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
+    
     $parameters->set(Option::PACKAGE_DIRECTORIES, [
         // base packages
         __DIR__ . '/packages',
